a8f2d8c3786dcd484553db9c58fe1d5e6713cc69
andriihovanec <andriihovanec@gmail.com>
2020-04-10 13:39:14 +0300

    initial commit

---
+++ src/main/java/com/khovanets/beautyshop/BeautyShopApplication.java
@@ -0,0 +1,8 @@
+package com.khovanets.beautyshop;
+
+public class BeautyShopApplication {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/test/java/com/khovanets/beautyshop/BeautyShopApplicationTest.java
@@ -0,0 +1,5 @@
+package com.khovanets.beautyshop;
+
+class BeautyShopApplicationTest {
+
+}
